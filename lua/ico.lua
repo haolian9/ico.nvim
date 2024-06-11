4a97bfd6331aa41306ca05e609b28e0b663d9ed2
haoliang <haoliang0.1.2@gmail.com>
2024-06-11 11:34:42 +0800

    sync with upstream

--- lua/ico.lua
+++ lua/ico.lua
@@ -1,4 +1,3 @@
-local ex = require("infra.ex")
 local feedkeys = require("infra.feedkeys")
 local jelly = require("infra.jellyfish")("ico", "debug")
 local tty = require("infra.tty")
@@ -11,8 +10,6 @@ local ascii = {
   --
   t = string.byte("t"),
   f = string.byte("f"),
-  T = string.byte("T"),
-  F = string.byte("F"),
 }
 
 local function is_inputable(code)
@@ -24,34 +21,25 @@ end
 ---@param mode 'f'|'t'
 local function ft(mode)
   local char, code = tty.read_raw()
-  if not is_inputable(code) then return end
+  if not is_inputable(code) then return jelly.warn("unexpected (%s, %d) for %schar", char, code, mode) end
   feedkeys.codes(mode, "n")
   feedkeys.keys(char, "m")
   feedkeys.codes("a", "n")
 end
 
----@param mode 'F'|'T'
-local function FT(mode)
-  local char, code = tty.read_raw()
-  if not is_inputable(code) then return end
-  feedkeys.codes(mode, "n")
-  feedkeys.keys(char, "m")
-  feedkeys.codes("i", "n")
-end
-
 ---designed to replace i_ctrl-o
 return function()
-  local char, code = tty.read_raw()
   feedkeys.keys("<esc>l", "n")
 
-  if code == ascii.esc then return end
+  vim.schedule(function() --schedule to allow redrawing the cursor
+    local char, code = tty.read_raw()
+    if code == ascii.esc then return end
 
-  if code == ascii.t then return ft("t") end
-  if code == ascii.f then return ft("f") end
-  if code == ascii.T then return FT("T") end
-  if code == ascii.F then return FT("F") end
+    if code == ascii.t then return ft("t") end
+    if code == ascii.f then return ft("f") end
 
-  --behave like the original <c-o>
-  feedkeys.keys("i<c-o>", "n")
-  feedkeys.keys(char, "m")
+    --behave like the original <c-o>
+    feedkeys.keys("i<c-o>", "n")
+    feedkeys.keys(char, "m")
+  end)
 end
